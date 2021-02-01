f6020c19fe391abf2faf3dfb429c318d08dc2eef
Bruno Cantero <brunocantero01@gmail.com>
2021-02-01 00:30:18 -0300

    fixed port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // Listen on a specific host via the HOST environment variable
-var host = process.env.HOST || 'localhost';
+var host = process.env.HOST || 'http://ec2-3-16-129-81.us-east-2.compute.amazonaws.com';
 // Listen on a specific port via the PORT environment variable
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 5000;
 
 // Grab the blacklist from the command-line so that we can update the blacklist without deploying
 // again. CORS Anywhere is open by design, and this blacklist is not used, except for countering
